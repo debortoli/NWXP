b741d6645650591a28c3f44e023e2b4e83f09d30
Bob <deborobe@gmail.com>
2017-03-11 22:23:20 -0800

    Added powerlines aging functionality. Added a very basic text update system.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@
 import pygame
 import numpy as np 
 import math
-from utils import Board,textUpdate,backgroundUpdate,draggablesUpdate
+from utils import Board
+import time
 
 
 
@@ -11,16 +12,23 @@ if __name__ == '__main__':
     board=Board()
     # Loop until the user clicks close button
     done = False
+    board.clock.tick()
     while done == False:
-        
+        print int(pygame.time.get_ticks()/1000.)
+        if(int(pygame.time.get_ticks()/1000.)%10==0 and 
+            (pygame.time.get_ticks()/1000.)>1):
+            board.year+=1
+            print "here"
         # write event handlers here
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # print "button clicked"
+                # print event.pos
                 for icon in board.dnd:
                     if icon.rect.collidepoint(event.pos):
+
                         board.update_text+="item is being dragged"+'\n'
                         # print "clicked on icon"
                         
@@ -29,11 +37,14 @@ if __name__ == '__main__':
                 for icon in board.dnd:
                     icon.click = False
         # write game logic here
-     
+        # if(board.year%10==0 and board.year>0):
+        #     board.update_text+="Another 10 years have passed.\n"
+
         
-        backgroundUpdate(board)
-        textUpdate(board,board.update_text)
-        draggablesUpdate(board)
+        board.staticItemsUpdate()
+        board.textUpdate()
+        board.draggablesUpdate()
+        board.checkPowerLineAge()
         
         # write draw code here
      
@@ -41,6 +52,7 @@ if __name__ == '__main__':
         pygame.display.update()
         # run at 20 fps
         board.clock.tick(20)
+        # time.sleep(2)
      
     # close the window and quit
     pygame.quit()
--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,5 @@
 import pygame
 import planes
-
-class Background(pygame.sprite.Sprite):
-	def __init__(self, image_file, location):
-		pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
-		self.image = pygame.image.load(image_file)
-		self.rect = self.image.get_rect()
-		self.rect.left, self.rect.top = location
-
 class Board:
 	def __init__(self):
 		# initialize game engine
@@ -26,31 +18,100 @@ class Board:
 		self.dnd=[]
 		d=DragNDrop((0,900,50,50),'../images/city.jpg')
 		self.dnd.append(d)
-		self.update_text=""
+		self.update_text="Game has begun!\n"
+		self.year=0
+		self.createCities()
+		self.createTransmissionLines()
 
-def textUpdate(board,input_text):
-	textsurface = board.myfont.render(input_text, False, (0, 0, 0))
-	board.screen.blit(textsurface,(621,0))
+	def createCities(self):
+		self.cities=[]
+		c=City('../images/city_small.jpg',[110,180])
+		self.cities.append(c)
+		c=City('../images/city_small.jpg',[93,240])
+		self.cities.append(c)
 
-def backgroundUpdate(board):
-	board.screen.fill([255, 255,255])
-	board.screen.blit(board.BackGround.image, board.BackGround.rect)
+	def createTransmissionLines(self):
+		self.transmissionLines=[]
+		t=Transmission('../images/transmission_small.png',[90,205],
+			"PortlandCorvallis",0)
+		self.transmissionLines.append(t)
 
-def draggablesUpdate(board):
-	for i,icon  in enumerate(board.dnd):
-		# print board.dnd
-		icon.update(board.screen)
-		# board.screen.blit(icon,(i*100,700))
-	return None
-	# city_im=pygame.image.load()
-	# p=planes.Plane("City",city_im)
+	def textUpdate(self):
+		#format text
+		line=""
+		line_num=0
+		for char in self.update_text:
+			if char=='\n':
+				t=self.myfont.render(line, False, (0, 0, 0))
+				self.screen.blit(t,(621,0+20*line_num))
+				line=""
+				line_num+=1
+			else:
+				line+=char
+
+	def staticItemsUpdate(self):
+		#background image
+		self.screen.fill([255, 255,255])
+		self.screen.blit(self.BackGround.image, self.BackGround.rect)
+
+		#cities
+		for city in self.cities:
+			self.screen.blit(city.image,city.rect)
+
+		#powerlines
+		for line in self.transmissionLines:
+			line.update(self)
+			self.screen.blit(line.image,line.rect)
+
+	def draggablesUpdate(self):
+		for i,icon  in enumerate(self.dnd):
+			icon.update(self.screen)
+			# board.screen.blit(icon,(i*100,700))
+
+	def checkPowerLineAge(self):
+		for line in self.transmissionLines:
+			# print self.update_text[(len(str(line.name))+1)*-1:-1]
+			if(line.age>15 and
+				self.update_text[(len(str(line.name))+1)*-1:-1]!=str(line.name)):
+				self.update_text+="Powerline out of date: "+str(line.name)+"\n"
+
+
 
 class DragNDrop:
-    def __init__(self,rect,filename):
-        self.rect = pygame.Rect(rect)
-        self.click = False
-        self.image = pygame.image.load(filename)
-    def update(self,surface):
-        if self.click:
-            self.rect.center = pygame.mouse.get_pos()
-        surface.blit(self.image,self.rect)
+	def __init__(self,rect,filename):
+		self.rect = pygame.Rect(rect)
+		self.click = False
+		self.image = pygame.image.load(filename)
+	def update(self,surface):
+		if self.click:
+			self.rect.center = pygame.mouse.get_pos()
+		surface.blit(self.image,self.rect)
+
+
+class Background(pygame.sprite.Sprite):
+	def __init__(self, image_file, location):
+		pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+		self.image = pygame.image.load(image_file)
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = location
+
+class City(pygame.sprite.Sprite):
+	def __init__(self, image_file, location):
+		pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+		self.image = pygame.image.load(image_file)
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = location
+
+class Transmission(pygame.sprite.Sprite):
+	def __init__(self, image_file, location,name,age):
+		pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+		self.image = pygame.image.load(image_file)
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = location
+		self.initial_age=age
+		self.age=age
+		self.name=name
+	def update(self,board):
+		self.age=self.initial_age+board.year
+
+	
